535a3539592a6ed8933ed65c3c109080c3ab4f81
Steve Long <steven989@gmail.com>
2014-04-15 20:01:12 -0400

    added more features

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,13 +18,55 @@ class ProductsController < ApplicationController
         
     end 
 
+    def new
+    
+        @product = Product.new
+
+    end 
+
+    def create
+
+    end 
+
+
     def user_index
 
         @products = User.find_by(id: session[:user_id]).products
 
-        
+    end 
+
+    def edit
+
+        @product = Product.find_by(id: params[:id])
 
     end 
 
+    def update
+
+        @product = Product.find_by(id: params[:id])
+
+        if @product.update_attributes(product_params)
+            redirect_to products_path
+        else 
+            render action: :edit
+        end 
+
+    end 
+
+    def destroy
+
+        Product.destroy(params[:id])
+
+        redirect_to products_path
+
+    end 
+
+    private
+
+    def product_params
+
+        params.require(:product).permit(:name,:category,:price,:description,:image_url)
+
+    end 
 
 end
